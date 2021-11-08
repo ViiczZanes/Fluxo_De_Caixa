0546fb70f76a041306dd0a5a351c7b83ffab9852
yIguuh47 <epic.47.54.19@gmail.com>
2021-11-08 16:47:05 -0300

    login feito

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,26 @@
-const log_out = () => {
-    confirm('Tem Certeza Que Deseja Sair?')
+class Login{
+    logar(){
+        const txtUser = document.getElementById("txUser").value
+        const txtPass = document.getElementById("txPassword").value
     
+        if(txtUser && txtPass == "admin"){
+            window.location.href = "../views/Ganhos.html"
+            alert("Foi!")
+        } else {
+            alert("Erro tente novamente")
+        }
+    
+    }
+
+    log_out(){
+       const confi =  confirm("Deseja Realmente Sair?")
+        if(confi){
+            window.location.href = "../views/index.html"
+        }else{
+            alert("Bem Vindo de Volte 🥺❤️")
+        }
+    }
+
 }
+
+const login = new Login()
